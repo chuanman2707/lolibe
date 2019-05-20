8251873a86fd18d213feb9a2582644c1e6e3f876
chuanman2707 <chuanman2707@gmail.com>
2019-05-20 10:50:58 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import styled from "styled-components"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h3>hello from index</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">hello world</div>
+        <ButtonWrapper>Click Me</ButtonWrapper>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
+const ButtonWrapper = styled.button`
+  background: blue;
+  color: white;
+`
 export default IndexPage
